bb581f4ebfa15242a26ca9a8866318a04ecf8b0f
AnantUpadhyay02 <amanupadhyay1719@gmail.com>
2023-10-01 00:13:29 +0530

    Object oriented Programming

--- OOPS_/OOPS.cpp
+++ OOPS_/OOPS.cpp
@@ -9,22 +9,50 @@ using namespace std;
 class hero {
     //Properties
     private: // Access modifier: Private --> Acessable only in the private modifier
-    string name = "Rocky";
-    void print(){
-        cout<< name;
-    }
+    int health;
+    char level;
+    string game;
 
 
     public: // Access modifier: Public --> Accesable anywhere in the program
-    int health;
-    char level;
+    string name;
+
+    // Getter and setter: 
+    int gethealth(){
+        return health;
+    }
+    void sethealth(int h) {
+        health = h;
+    }
+
+    char getlevel(){
+        return level;
+    }
+    void setlevel(char l){
+        level = l;
+    }
+    void setgame(string a){
+        if(a == "Boxing"){  // Condition in setter, to give access to specific keyword/person, kinda like a password.
+            game = a;
+        }
+    }
+    string getgame(){
+        return game;
+    }
 };
 
 
 int main(){
     hero h1;
-    cout<<"size :"<<sizeof(h1)<<endl; 
-    cout<<"level :"<<h1.level<<endl;
+    h1.name = "Rocky";
+    h1.setlevel('A');
+    h1.sethealth(100);
+    h1.setgame("MMA");
+    // cout<<"size :"<<sizeof(h1)<<endl; 
+    cout<<"Name :"<<h1.name<<endl;
+    cout<<"level :"<<h1.getlevel()<<endl;
+    cout<< "Health :"<<h1.gethealth()<<endl;
+    cout<<"Game :"<<h1.getgame()<<endl;
 
 return 0;
 }
