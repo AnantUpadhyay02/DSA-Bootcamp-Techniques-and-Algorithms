119e9ca5f8630fc20df80309346dd59888cc3501
AnantUpadhyay02 <amanupadhyay1719@gmail.com>
2023-11-22 23:08:19 +0530

    Update Maps.cpp

--- Maps.cpp/Maps.cpp
+++ Maps.cpp/Maps.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
+#include<map>
+#include<unordered_map>
 using namespace std;
 
 
 int main(){
+    // Creating
+    unordered_map<string, int> m;
+
+    // inserting 
+    pair<string,int> p = make_pair("anant",3);
+    m.insert(p);
+
+    pair<string, int> pair2("Upadhyay",2); // one way of initialising
+    m.insert(pair2);// another way of initialising
+
+    m["Mera"] = 1;//another way of initialising
+
 
 return 0;
 }
